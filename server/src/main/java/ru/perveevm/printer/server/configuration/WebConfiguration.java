d6a07bc275190312227b2b174b7a81d2c60e5bde
Perveev Mike <perveev_m@mail.ru>
2022-12-04 09:01:06 +0300

    CORS disabled

---
+++ server/src/main/java/ru/perveevm/printer/server/configuration/WebConfiguration.java
@@ -0,0 +1,13 @@
+package ru.perveevm.printer.server.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
+    }
+}
